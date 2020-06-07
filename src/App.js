12408c8885487b615fb0b4322539b9a475cd0c5f
aditgiant <adityagianto@gmail.com>
2020-06-07 15:22:14 +0200

    fix link

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ class App extends Component {
       <Sidebar/>
       <Switch>
       <Route path={process.env.PUBLIC_URL + '/'} exact component={Display}/>
-      <Route path={process.env.PUBLIC_URL + '/aboutus'} exact component={About}/>
-      <Route path={process.env.PUBLIC_URL + '/collections'} component={Lookbook}/>
-      <Route path={process.env.PUBLIC_URL + '/product/:id'} component={Product}/>
-      <Route path={process.env.PUBLIC_URL + '/admin'} component={Admin}/>
-      <Route path={process.env.PUBLIC_URL + '/adminedit/:id'} component={AdminEdit} />
-      <Route path={process.env.PUBLIC_URL + '/admincreate'} component={AdminCreate} />
-      <Route path={process.env.PUBLIC_URL + '/adminshow/:id'} component={AdminShow} />
+      <Route path={process.env.PUBLIC_URL + '/jelujur/aboutus'} exact component={About}/>
+      <Route path={process.env.PUBLIC_URL + '/jelujur/collections'} component={Lookbook}/>
+      <Route path={process.env.PUBLIC_URL + '/jelujur/product/:id'} component={Product}/>
+      <Route path={process.env.PUBLIC_URL + '/jelujur/admin'} component={Admin}/>
+      <Route path={process.env.PUBLIC_URL + '/jelujur/adminedit/:id'} component={AdminEdit} />
+      <Route path={process.env.PUBLIC_URL + '/jelujur/admincreate'} component={AdminCreate} />
+      <Route path={process.env.PUBLIC_URL + '/jelujur/adminshow/:id'} component={AdminShow} />
       </Switch>
     </Router>
     </div>
