34efa4c78366d0da5540ce695698b3add237e4db
aditgiant <adityagianto@gmail.com>
2020-07-01 15:31:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jelujur/precache-manifest.7d1c6e21b38a6264bc71ca22977ebbc2.js"
+  "/jelujur/precache-manifest.d2bb1c5dac187195d820a7e2f4ce3924.js"
 );
 
 self.addEventListener('message', (event) => {
